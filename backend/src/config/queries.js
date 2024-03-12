366b3617581c52892921dd747c6e59e27b547e39
anupsreedhar <anup.sreedhaar@gmail.com>
2024-03-12 09:43:04 +0530

    Updated

--- backend/src/config/queries.js
+++
@@ -1,8 +0,0 @@
-const getByEmployeeId = (req, resp) => {
-    const id = parseInt(req.params.id);
-    pool.query(queries.getEmployeeById, [id], (error,results) => {
-        if(error) throw error;
-        resp.status(200).send(results.rows);
-        console.log(`Employee with ${id} returned successfully`);
-    });
-}
